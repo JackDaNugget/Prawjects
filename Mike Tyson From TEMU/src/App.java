8f08080cf9029b38930117a18bdf8d3a7d0da8cb
JackDaNugget <108022312+JackDaNugget@users.noreply.github.com>
2025-02-20 18:08:53 +0000

    sfsxcd

--- Mike Tyson From TEMU/src/App.java
+++ Mike Tyson From TEMU/src/App.java
@@ -1,5 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, Cruel World! I've missed you so much.");
-    }
+    System.out.println("Hello, Cruel World! I've missed you so much.");
+     System.out.println("Lmao just kidding I actually miss Club Penguin");
+}
 }
